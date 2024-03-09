06465fc51ffb212113810cbe8b3e2c787899980a
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2024-03-09 16:54:02 +0000

    Basic Bonzo test setup done

---
+++ test/addresses-provider.spec.ts
@@ -0,0 +1,20 @@
+import exp from 'constants';
+
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+import { LendingPool, LendingPoolAddressesProvider } from '../deployed-contracts.json';
+
+describe('Addresses Provider', function () {
+  it('Should read addresses from the addresses provider', async function () {
+    const [owner] = await ethers.getSigners();
+    const contractArtifacts = await hre.artifacts.readArtifact('LendingPoolAddressesProvider');
+    const abi = contractArtifacts.abi;
+
+    const contractAddress = LendingPoolAddressesProvider.hedera_testnet.address;
+
+    const tokenContract = new ethers.Contract(contractAddress, abi, owner);
+    const lendingPoolAddress = await tokenContract.getLendingPool();
+    console.log('Lending Pool Address: ', lendingPoolAddress);
+    expect(lendingPoolAddress).to.equal(LendingPool.hedera_testnet.address);
+  });
+});
---
+++ test/lending-pool.spec.ts
@@ -0,0 +1,35 @@
+import exp from 'constants';
+
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+import { LendingPool, LendingPoolAddressesProvider } from '../deployed-contracts.json';
+
+describe('Addresses Provider', function () {
+  it('Should get the addresses provider from lending pool', async function () {
+    const [owner] = await ethers.getSigners();
+    const contractArtifacts = await hre.artifacts.readArtifact('LendingPool');
+    const abi = contractArtifacts.abi;
+
+    const contractAddress = LendingPool.hedera_testnet.address;
+
+    const tokenContract = new ethers.Contract(contractAddress, abi, owner);
+    const lendingPoolAddress = await tokenContract.getAddressesProvider();
+    console.log('Lending Pool Addresses provider: ', lendingPoolAddress);
+    expect(lendingPoolAddress).to.equal(LendingPoolAddressesProvider.hedera_testnet.address);
+  });
+
+  it('should return reserve data', async function () {
+    const [owner] = await ethers.getSigners();
+    const contractArtifacts = await hre.artifacts.readArtifact('LendingPool');
+    const abi = contractArtifacts.abi;
+
+    const contractAddress = LendingPool.hedera_testnet.address;
+
+    const tokenContract = new ethers.Contract(contractAddress, abi, owner);
+    const reserveData = await tokenContract.getReserveData(
+      '0x0000000000000000000000000000000000120f46'
+    );
+    console.log('Reserve Data: ', reserveData);
+    expect(reserveData).to.not.be.null;
+  });
+});
